b4b2ccf3292a7c5389f5e6d64262cc9794270d37
Meryemnursikak <meryemnursikak@gmail.com>
2022-03-03 14:47:22 +0300

    Add project files.

---
+++ HelperMetotMVC/library/HtmlHelperClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperMetotMVC.library
+{
+    public static class HtmlHelperClass
+    {
+        public static MvcHtmlString Alert(this HtmlHelper helper,string renk,string metin, string id = "alert1")
+        {
+            TagBuilder tag = new TagBuilder("div");
+            tag.AddCssClass("alert alert-" + renk);
+            tag.GenerateId(id);
+            tag.Attributes.Add("role", "alert");
+            tag.SetInnerText(metin);
+            return MvcHtmlString.Create(tag.ToString());
+        }
+    }
+}
